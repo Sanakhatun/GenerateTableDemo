039cb2c0392f60703e2e1ab152f08363b7907008
Sanakhatun Shaikh <sanakhatun.shaikh@mobicule.com>
2021-12-11 14:08:13 +0530

    Resolved issue

--- app/src/main/java/com/sana/generatetabledemo/MainActivity.java
+++ app/src/main/java/com/sana/generatetabledemo/MainActivity.java
@@ -8,6 +8,8 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -15,9 +17,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int counter;
     private TableLayout tl_result, tl_overallResult;
-    private TableRow tableRow, tr_overallResult;
+    private TableRow tableRow;
     private TextView tv_overallResult;
+    private Button btn_click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +32,17 @@ public class MainActivity extends AppCompatActivity {
         tl_overallResult = findViewById(R.id.tl_overallResult);
         tableRow = findViewById(R.id.tableRow);
         tv_overallResult = findViewById(R.id.tv_overallResult);
+        btn_click = findViewById(R.id.btn_click);
 
-        displayResultView(this, tl_result, tableRow, tl_overallResult, tv_overallResult);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tl_result.setVisibility(View.VISIBLE);
+                displayResultView(MainActivity.this, tl_result, tableRow, tl_overallResult, tv_overallResult);
+            }
+        });
+
+
     }
 
     public static void displayResultView(Context context, TableLayout tableLayout, TableRow tableRow, TableLayout tl_overallResult, TextView tv_overallResult) {
@@ -38,6 +51,9 @@ public class MainActivity extends AppCompatActivity {
 
             if (tableLayout != null) {
 
+                tableLayout.removeAllViews();
+
+                Log.i("tableLayout Count : ", "" + tableLayout.getChildCount());
                 TableLayout tl_part1 = new TableLayout(context);
 
                 tl_part1.addView(drawTableRow(context,
@@ -48,10 +64,20 @@ public class MainActivity extends AppCompatActivity {
                 String overallResult = "Pass";
                 for (int i = 0; i < 3; i++) {
                     String name = "Sana Shaikh", grade = "A";
-                    tl_part1.addView(drawTableRow(context, String.valueOf(i + 1), name, grade, false));
+                    tl_part1.addView(drawTableRow(context, String.valueOf(i + 1), name, grade + (counter++), false));
+                }
+
+                if (tableRow != null) {
+                    Log.i("tableRow Count : ", "" + tableRow.getChildCount());
+                    tableRow.removeAllViews();
+                    Log.i("tableRow Count : ", "" + tableRow.getChildCount());
                 }
 
                 tableRow.addView(tl_part1, 0);
+
+                if(tl_overallResult != null){
+                    tl_overallResult.removeAllViews();
+                }
 
                 TableRow tr_overallResult = new TableRow(context);
                 TextView tv_overallResultHeader = createTextView(context, "Overall Result(Pass/Fail)", R.drawable.border_layout, 16f, Gravity.CENTER);
@@ -60,7 +86,10 @@ public class MainActivity extends AppCompatActivity {
 
                 tl_overallResult.addView(tr_overallResult, 0);
 
-                tv_overallResult.setText(overallResult);
+                tableRow.addView(tl_overallResult);
+
+                tv_overallResult.setText(overallResult+counter);
+                tl_overallResult.addView(tv_overallResult);
 
                 tableLayout.addView(tableRow);
 
